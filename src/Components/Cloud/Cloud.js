38bf5479920675797abc0edb2693b36c0d2a184e
Victor Lebrun <victorl@theodo.fr>
2019-07-25 22:18:22 +0200

    Add first cloud

---
+++ src/Components/Cloud/Cloud.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Cloud = () => (
+  <div style={{ position: 'relative' }}>
+    <div className="cloud" />
+    <div className="surrimage">
+      <div className="rimage">Nuage</div>
+    </div>
+  </div>
+);
+
+export default Cloud;
