c89191336bc6771bf1f3e555cb4a366d17672334
Victor Lebrun <victorl@theodo.fr>
2019-07-26 19:02:22 +0200

    Updates

---
+++ precache-manifest.fdb8b0a740a586209e82cdc1a7a4cd7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df90c1d496c3f52d163bc6e864429b1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "307635b723bd47e00f75",
+    "url": "/static/css/main.99ede1a4.chunk.css"
+  },
+  {
+    "revision": "cf2f527e882ebcd03947",
+    "url": "/static/js/2.b84a0f17.chunk.js"
+  },
+  {
+    "revision": "307635b723bd47e00f75",
+    "url": "/static/js/main.04f1da72.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/Components/Ciel/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Ciel } from './Ciel';
