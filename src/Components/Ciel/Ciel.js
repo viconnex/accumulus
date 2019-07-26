3b7b1f03c66c733bf9f7570a4ccec815cb1da68f
Victor Lebrun <victorl@theodo.fr>
2019-07-26 18:38:35 +0200

    Use Tweenlite

--- src/Components/Ciel/Ciel.js
+++ src/Components/Ciel/Ciel.js
@@ -1,36 +1,9 @@
 import React from 'react';
+import Cumulus2 from '../Cumulus/Cumulus';
 import './style.css';
-import styled, { keyframes } from 'styled-components';
 
-const cloudHeight = 100;
-const getChute = altitude => window.innerHeight - altitude;
-
-const fallingKeyFrame = chute => keyframes`
-    from {
-        transform: translateY(-100px);
-    }
-    to {
-        transform: translateY(${chute}px);
-    }
-`;
-
-const StyledCumulus = styled.div`
-  animation-name: ${props => fallingKeyFrame(getChute(props.altitude))};
-  animation-duration: 5s;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-`;
-
-const Cumulus = ({ altitude }) => {
-  return (
-    <StyledCumulus className="cumulus" altitude={altitude}>
-      <div className="cloud" />
-      <div className="surrimage">
-        <div className="rimage">Nuage</div>
-      </div>
-    </StyledCumulus>
-  );
-};
+const cloudBaseHeight = 100;
+const cloudHeight = 170;
 
 const Ciel = () => {
   const [clouds, setClouds] = React.useState([]);
@@ -46,9 +19,8 @@ const Ciel = () => {
         Nuage
       </button>
       {clouds.map(cloudIndex => {
-        const altitude = (cloudIndex + 1) * cloudHeight;
-        console.log(altitude);
-        return <Cumulus key={cloudIndex} altitude={altitude} />;
+        const chute = window.innerHeight - cloudIndex * cloudHeight - cloudBaseHeight;
+        return <Cumulus2 chute={chute} />;
       })}
     </div>
   );
