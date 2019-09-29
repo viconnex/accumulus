091ade45c6a04b6f1e9fcbc3b1ae1abbfae7f8d8
Victor Lebrun <victorl@theodo.fr>
2019-09-29 22:07:46 +0200

    Fix app router path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import { Ciel } from './Components/Ciel';
 import { Musiciel } from 'Components/Musiciel';
@@ -8,10 +8,10 @@ function App() {
   return (
     <Router basename="/">
       <div className="App">
-        {/* <header className="App-header"> */}
-        <Route exact path="/cloud-nine" render={() => <Ciel cloud9 />} />
-        <Route exact path="/" component={() => <Musiciel />} />
-        {/* </header> */}
+        <Switch>
+          <Route path="/cloud-nine" render={() => <Ciel cloud9 />} />
+          <Route exact path="/" component={() => <Musiciel />} />
+        </Switch>
       </div>
     </Router>
   );
