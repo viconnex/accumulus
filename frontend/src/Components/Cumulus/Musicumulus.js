43ae9f170747840ad36f294930a55db4561f0085
Victor Lebrun <victorl@theodo.fr>
2019-10-06 15:27:21 +0200

    Try notes in musiciel

--- frontend/src/Components/Cumulus/Musicumulus.js
+++ frontend/src/Components/Cumulus/Musicumulus.js
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { TweenLite, TweenMax } from 'gsap/TweenMax';
 import { random } from 'utils/helpers';
 import { Nuage } from './Nuage';
+import Tone from 'tone';
 import './style.css';
 
+const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+const getNote = (musicSheet, index) => {
+  return `${notes[Math.floor(musicSheet[index].note * notes.length)]}${index}`;
+};
 // TweenLite.defaultEase = Linear.easeNone;
 
 const chuteMax = window.innerHeight - 10;
@@ -19,23 +24,37 @@ const Musicumulus = ({ handleSkyLanding, nuageName, musicSheet, deriveMax }) => 
   }, [isArrived]);
 
   useEffect(() => {
+    const synth = new Tone.Synth().toMaster();
+
     TweenLite.set(cumulus, {
       x: musicSheet[0].note * deriveMax,
       y: chuteMax,
     });
 
-    const blowUp = cumulus => () => {
+    const blowUp = (cumulus, note) => () => {
       TweenMax.to(cumulus, 1, {
         y: -300,
-        onComplete: () => arrive(true),
+        onStart: () => synth.triggerAttack(note),
+        onComplete: () => {
+          arrive(true);
+          synth.triggerRelease();
+        },
       });
     };
 
     const twinTo = (index, cumulus) => () => {
-      TweenMax.to(cumulus, random(0.4, 2), {
+      TweenMax.to(cumulus, random(1, 3), {
         x: musicSheet[index].note * deriveMax,
         y: musicSheet[index].chordAltitude,
-        onComplete: index < musicSheet.length - 1 ? twinTo(index + 1, cumulus) : blowUp(cumulus),
+        onStart: () => {
+          if (index > 0) synth.triggerAttack(getNote(musicSheet, index - 1));
+        },
+        onComplete: () => {
+          // synth.triggerRelease();
+
+          if (index < musicSheet.length - 1) twinTo(index + 1, cumulus)();
+          else blowUp(cumulus, getNote(musicSheet, index))();
+        },
         // ease: Power4.easeOut,
       });
     };
